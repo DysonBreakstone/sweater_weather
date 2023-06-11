264abd524db222b082d1bc1b6ab028477775c6d4
DysonBreakstone <121531791+DysonBreakstone@users.noreply.github.com>
2023-06-11 17:01:02 -0600

    forecast poro

---
+++ spec/poros/forecast_spec.rb
@@ -0,0 +1,19 @@
+require 'rails_helper'
+
+RSpec.describe "forecast poro" do
+  describe "instantiation and attributes" do
+    before do
+      @forecast = ForecastFacade.new("Burlington,VT").create_forecast
+    end
+
+    it "exists" do
+      expect(@forecast).to be_a(Forecast)
+    end
+    
+    it "has attributes" do
+      expect(@forecast.current_weather).to be_a(Hash)
+      expect(@forecast.daily_weather).to be_a(Array)
+      expect(@forecast.hourly_weather).to be_a(Array)
+    end
+  end
+end
